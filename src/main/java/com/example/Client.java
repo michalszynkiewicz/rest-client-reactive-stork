919fb11d9dbc3c0bd9b7338d160bdf3649e237f7
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-11-09 12:42:35 +0100

    initial

---
+++ src/main/java/com/example/Client.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/test")
+@RegisterRestClient(baseUri = "http://localhost:8406")
+public interface Client {
+    @GET
+    String get();
+}
