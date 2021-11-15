9a6042bda84118d302734bf2862c0657937ece52
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-11-15 11:50:46 +0100

    demo start

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -7,7 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/test")
-@RegisterRestClient(baseUri = "stork://my-service")
+@RegisterRestClient(baseUri = "http://localhost:8406")
 public interface Client {
     @GET
     @Path("/")
